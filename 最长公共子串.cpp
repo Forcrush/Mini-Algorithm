199fae9efecff322c3e89f164baeee1a27754296
Forcrush <873729746@qq.com>
2018-08-09 22:22:43 +0800

    最长公共子串

---
+++ 最长公共子串.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int lena, lenb;
+    cout<<"String A length:";
+    cin>>lena;
+    cout<<"String B length:";
+    cin>>lenb;
+    int A[lena];
+    int B[lenb];
+    cout<<"Input A:";
+    for(int i=0;i<lena;i++) cin>>A[i];
+    cout<<"Input B:";
+    for(int i=0;i<lenb;i++) cin>>B[i];
+
+    int c[lena+1][lenb+1];
+    for (int i=0; i<lena + 1; i++){
+        for(int j=0; j<lenb + 1; j++){
+            c[i][j] = 0;
+        }
+    }
+
+    int res = 0;
+    for (int i=1; i<lena + 1; i++){
+        for (int j=1; j<lenb + 1; j++){
+            if (A[i - 1] == B[j - 1]){
+                c[i][j] = c[i - 1][j - 1] + 1;
+            }
+            res = max(res, c[i][j]);
+        }
+    }
+    cout<<res;
+    return 0;
+}
